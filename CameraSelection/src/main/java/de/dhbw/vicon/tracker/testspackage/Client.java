80e18a5f8eff0955063e8acd35f13698105babf5
rahm- <rahm-@MSI>
2023-07-27 13:33:03 +0200

    No more deprecated libraries

--- CameraSelection/src/main/java/de/dhbw/vicon/tracker/testspackage/Client.java
+++ CameraSelection/src/main/java/de/dhbw/vicon/tracker/testspackage/Client.java
@@ -8,24 +8,30 @@ package de.dhbw.vicon.tracker.testspackage;
  *
  * @author andres
  */
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
 
 public class Client {
     public static void main(String[] args) {
-        try (ZMQ.Context context = ZMQ.context(1);
-             ZMQ.Socket socket = context.socket(ZMQ.REQ)) {
+        try (ZContext context = new ZContext()) {
+            // Create a socket of type REQ (request)
+            ZMQ.Socket socket = context.createSocket(SocketType.REQ);
 
-            // Connect the socket to the server's address and port
+            // Connect the socket to the server
             // socket.connect("tcp://10.172.2.129:5555");  // Andres IP
-            socket.connect("tcp://10.172.15.77:5555");  // Lab IP
+            socket.connect("tcp://localhost:5555");  // Lab IP
 
-            // Send a request to the server
-            String requestMsg = "Hello from Client";
-            socket.send(requestMsg.getBytes(), 0);
-            
-            // Wait for the response from the server
-            byte[] response = socket.recv(0);
-            System.out.println("Received response: " + new String(response));
+            for (int i = 0; i < 5; i++) {
+                String message = "Request " + i;
+                // Send a request to the server
+                socket.send(message.getBytes(), 0);
+                System.out.println("Sent: " + message);
+
+                // Wait for the response from the server
+                byte[] reply = socket.recv(0);
+                System.out.println("Received: " + new String(reply));
+            }
         }
     }
 }
--- CameraSelection/src/main/java/de/dhbw/vicon/tracker/testspackage/Server.java
+++ CameraSelection/src/main/java/de/dhbw/vicon/tracker/testspackage/Server.java
@@ -4,16 +4,19 @@
  */
 package de.dhbw.vicon.tracker.testspackage;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
 
 public class Server {
     public static void main(String[] args) {
-        try (ZMQ.Context context = ZMQ.context(1);
-             ZMQ.Socket socket = context.socket(ZMQ.REP)) {
+        try (ZContext context = new ZContext()) {
+            // Create a socket of type REP (reply)
+            ZMQ.Socket socket = context.createSocket(SocketType.REP);
 
             // Bind the socket to a specific address and port
             // socket.bind("tcp://10.172.2.129:5555");  // Andres IP
-            socket.bind("tcp://10.172.15.77:5555");  // Lab IP
+            socket.bind("tcp://localhost:5555");  // Lab IP
 
             while (!Thread.currentThread().isInterrupted()) {
                 byte[] request = socket.recv(0); // Wait for a request from the client
