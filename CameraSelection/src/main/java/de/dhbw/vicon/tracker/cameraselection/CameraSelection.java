a4c3010286ba73a73ede978138dc8c2170226095
rahm- <rahm-@MSI>
2023-08-11 10:49:28 +0200

    Revert "Andres First Commit with SSH Keys"
    This reverts commit a53b71a99b5355fddec64c5c71d8d15b84bf1d12.

--- CameraSelection/src/main/java/de/dhbw/vicon/tracker/cameraselection/CameraSelection.java
+++ CameraSelection/src/main/java/de/dhbw/vicon/tracker/cameraselection/CameraSelection.java
@@ -12,6 +12,5 @@ public class CameraSelection {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Andres First Commit with keys");
     }
 }
